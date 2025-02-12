4f140f3ca49fd3e120aaed5b4d840de49baf49bc
sanachihi <sana.chihi@esprit.tn>
2025-02-12 12:39:16 +0100

    add attribut email sur mabranch

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -28,7 +28,7 @@ public class User implements Serializable {
 	private String firstName; 
 	
 	private String lastName;
-	
+	 private String email;//ajouter sur mabranch
 	@Temporal(TemporalType.DATE)
 	private Date dateNaissance;
 	
